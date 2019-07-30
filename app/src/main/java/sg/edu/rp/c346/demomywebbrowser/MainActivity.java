df957036619171ce1c1d6aaeeacd4d78d1d3cabe
OraclePearl <50016321+OraclePearl@users.noreply.github.com>
2019-07-30 12:57:59 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/demomywebbrowser/MainActivity.java
@@ -0,0 +1,40 @@
+package sg.edu.rp.c346.demomywebbrowser;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnLoadURL;
+    WebView wvMyPage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnLoadURL = findViewById(R.id.buttonLoad);
+        wvMyPage = findViewById(R.id.webViewMyPage);
+
+        WebSettings websetting = wvMyPage.getSettings();
+        websetting.setJavaScriptEnabled(true);
+        websetting.setAllowFileAccess(false);
+        websetting.setBuiltInZoomControls(true);
+
+        btnLoadURL.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "https://www.grab.com/sg/";
+                wvMyPage.loadUrl(url);
+            }
+        });
+
+
+
+
+    }
+}
